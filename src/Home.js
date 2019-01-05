bd38b6da8f07afc7dd6d09be55ac8efdbdf06956
georgruetsche <georg.ruetsche@hotmail.com>
2019-01-05 12:00:00 +0100

    initalize project

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>You probably haven't heard of 
+          them activated charcoal blue 
+          bottle man bun ramps occupy 
+          succulents retro narwhal seitan 
+          church-key subway tile chartreuse 
+          cliche woke. Pork belly intelligentsia 
+          readymade, helvetica skateboard lomo
+           echo park. Art party 8-bit bespoke,
+           shoreditch organic offal marfa la 
+           croix wolf fashion axe chicharrones 
+           artisan seitan. </p>
+ 
+        <p>Lyft microdosing 
+           locavore pickled, pabst deep v prism 
+           live-edge food truck authentic. 
+           Single-origin.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Miscellaneous</h2>
+        <p>Lorem ipsum dolor amet before they sold out knausgaard leggings semiotics farm-to-table blue bottle. Venmo slow-carb plaid, raclette four loko polaroid flexitarian four dollar toast lyft affogato. Activated charcoal fixie iPhone ugh cray, plaid hammock offal next level edison bulb disrupt cronut taiyaki kale chips distillery. Forage cray squid, gochujang man bun hammock slow-carb fingerstache four loko schlitz coloring book kombucha twee mumblecore meh. Listicle synth kale chips, vice asymmetrical jean shorts photo booth keffiyeh ethical banjo poutine leggings single-origin coffee.:</p>
+        <ol>
+          <li>Nuttela</li>
+          <li>Schoggi</li>
+          <li>Nidelzelti</li>
+          <li>Linth</li>
+          <li>Pudding</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
